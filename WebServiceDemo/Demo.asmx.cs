2374b7ab6719dd3f7e9a3b1d398c4ca29eede9e8
miltondev <miltondev@outlook.com>
2017-11-16 05:08:34 -0600

    first commit

---
+++ WebServiceDemo/Demo.asmx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServiceDemo
+{
+    /// <summary>
+    /// Descripción breve de Demo
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Demo : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+
+        [WebMethod]
+        public int Suma(int Num1 , int Num2)
+        {
+            return Num1 + Num2;
+        }
+    }
+}
